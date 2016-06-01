c0ef6730eadc9e4d3ba40c797fe658c959a4bca7
thomas_huchede <huchede.thomas@gmail.com>
2016-06-01 16:17:51 +0200

    Change server, now used for all the simulation part, no longer used for screenshot & kml

--- server.py
+++ server.py
@@ -1,32 +1,43 @@
-from flask import Flask, url_for, send_file
+from flask import Flask, url_for, send_file, request
 import os
 app = Flask(__name__)
 
-@app.route('/')
-def api_root():
-    return 'Welcome'
+@app.route('/create', methods=['POST'])
+def create():
+    if request.method == 'POST':
+        data = request.json
+        instance = data['instance']
+        longitude = data['home']['geopoint']['longitude']
+        latitude = data['home']['geopoint']['latitude']
+        altitude = data['home']['geopoint']['altitude']
+        os.system('dronekit-sitl copter --home=' + latitude + ','
+        + longitude + ',' + altitude + ',1 --instance=' + instance)
+        return 'OK', 200
+    return 'Error', 404
 
-@app.route('/screenshot')
-def api_screen():
-    os.system('echo LOL')
-    filename = 'screenshot.png'
-    return send_file(filename, mimetype='image/png')
+@app.route('/mission', methods=['POST'])
+def mission():
+    if request.method == 'POST':
+        data = request.json
+        ident = data['id']
+        intervention = data['intervention']
+        instance = data['instance']
+        geopoints = data['mission']['geopoints']
+        mission = ''
+        for point in range(len(geopoints)):
+            mission += geopoints[point]['latitude']
+            mission += ' '
+            mission += geopoints[point]['longitude']
+            mission += ' '
+            mission += geopoints[point]['altitude']
+            mission += ' '
+        print mission
+        os.system('python mission.py --id ' + ident
+        + ' --intervention ' + intervention + ' --instance' + instance
+        + ' --mission' + mission)
+        return 'OK', 200
+    return 'Error', 404
 
-@app.route('/get_image')
-def get_image():
-    # if request.args.get('type') == '1':
-    #    filename = 'ok.png'
-    # else:
-    #    filename = 'error.png'
-    filename = 'screenshot.png'
-    return send_file(filename, mimetype='image/png')
-# @app.route('/articles')
-# def api_articles():
-#     return 'List of ' + url_for('api_articles')
-#
-# @app.route('/articles/<articleid>')
-# def api_article(articleid):
-#     return 'You are reading ' + articleid
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
